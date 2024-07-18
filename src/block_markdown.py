13639083b82e0092bbab2faa8053991dd66314cd
Thomas Runyan <runyant13@gmail.com>
2024-07-17 20:14:02 -0400

    Added methods to determine block type

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,6 @@
+import re
+
+
 def markdown_to_blocks(markdown):
     return list(
         filter(
@@ -5,3 +8,39 @@ def markdown_to_blocks(markdown):
             map(lambda x: x.strip(), markdown.split("\n\n"))
             )
         )
+
+
+def block_to_block_type(block):
+    if is_heading(block):
+        return "heading"
+    elif is_codeblock(block):
+        return "code"
+    elif is_quote(block):
+        return "quote"
+    elif is_unordered_list(block):
+        return "unordered_list"
+    elif is_ordered_list(block):
+        return "ordered_list"
+    else:
+        return "paragraph"
+
+
+def is_heading(block):
+    headings = ["# ", "## ", "### ", "#### ", "##### ", "###### "]
+    return any(block.startswith(h) for h in headings)
+
+
+def is_codeblock(block):
+    return block.startswith("```") and block.endswith("```")
+
+
+def is_quote(block):
+    return all(line.startswith(">") for line in block.split("\n"))
+
+
+def is_unordered_list(block):
+    return all(line.startswith("* ") or line.startswith("- ") for line in block.split("\n"))
+
+
+def is_ordered_list(block):
+    return all(re.search(r"^\d+.\s", line) for line in block.split("\n"))
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,5 +1,5 @@
 import unittest
-from block_markdown import markdown_to_blocks
+from block_markdown import markdown_to_blocks, block_to_block_type
 
 
 class TestTextNode(unittest.TestCase):
@@ -49,6 +49,54 @@ It has some **bold** and *italic* words inside of it.
                 ],
                 result
         )
+
+    def test_block_to_blocktype_paragraph(self):
+        block = "This is just some random text"
+        expected = "paragraph"
+
+        result = block_to_block_type(block)
+
+        self.assertEqual(expected, result)
+
+    def test_block_to_blocktype_heading(self):
+        block = "### This is a heading"
+        expected = "heading"
+
+        result = block_to_block_type(block)
+
+        self.assertEqual(expected, result)
+
+    def test_block_to_blocktype_code(self):
+        block = "``` This is a code block ```"
+        expected = "code"
+
+        result = block_to_block_type(block)
+
+        self.assertEqual(expected, result)
+
+    def test_block_to_blocktype_quote(self):
+        block = ">Quote Line 1\n>Quote Line 2"
+        expected = "quote"
+
+        result = block_to_block_type(block)
+
+        self.assertEqual(expected, result)
+
+    def test_block_to_blocktype_unordered_list(self):
+        block = "* Line 1\n- Line 2"
+        expected = "unordered_list"
+
+        result = block_to_block_type(block)
+
+        self.assertEqual(expected, result)
+
+    def test_block_to_blocktype_ordered_list(self):
+        block = "1. Line 1\n2. Line 2"
+        expected = "ordered_list"
+
+        result = block_to_block_type(block)
+
+        self.assertEqual(expected, result)
 
 
 if __name__ == "__main__":
