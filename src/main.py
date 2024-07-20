07dfc24c8e9554b08d75454c9a689ead413b9887
Thomas Runyan <runyant13@gmail.com>
2024-07-20 09:00:34 -0400

    Fixed error in copy function

--- src/main.py
+++ src/main.py
@@ -12,22 +12,22 @@ def copy_src_to_public():
     if os.path.exists(dst_path):
         print("Deleting public dir...")
         shutil.rmtree(dst_path)
+
+    os.mkdir(dst_path)
+
+    if os.path.exists(src_path):
+        files_to_copy = os.listdir(src_path)
+
+        if len(files_to_copy) > 0:
+            for file in files_to_copy:
+                print("Copying " + file + "...")
+                shutil.copy(os.path.join(src_path, file), dst_path)
+
+                if os.path.isfile(os.path.join(dst_path, file)) is not True:
+                    raise OSError("Newly generated file not found")
+
     else:
-        os.mkdir(dst_path)
-
-        if os.path.exists(src_path):
-            files_to_copy = os.listdir(src_path)
-
-            if len(files_to_copy) > 0:
-                for file in files_to_copy:
-                    print("Copying " + file + "...")
-                    shutil.copy(os.join(src_path, file), dst_path)
-
-                    if os.isfile(os.path.join(dst_path, file)) is not True:
-                        raise OSError("Newly generated file not found")
-
-        else:
-            raise OSError("Source path does not exist")
+        raise OSError("Source path does not exist")
 
     print("File copy from src to public completed.")
 
