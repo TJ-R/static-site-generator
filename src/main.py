db69e8fa7018e8fa4e5b5695873e0e63542d0a49
Thomas Runyan <runyant13@gmail.com>
2024-07-20 08:56:43 -0400

    Changed src from src to static directory

--- src/main.py
+++ src/main.py
@@ -7,21 +7,27 @@ def main():
 
 
 def copy_src_to_public():
-    path = "./public"
-    if os.path.exists(path):
+    src_path = "./static"
+    dst_path = "./public"
+    if os.path.exists(dst_path):
         print("Deleting public dir...")
-        shutil.rmtree(path)
+        shutil.rmtree(dst_path)
     else:
-        os.mkdir(path)
-        files_to_copy = os.listdir()
+        os.mkdir(dst_path)
 
-        if len(files_to_copy) > 0:
-            for file in files_to_copy:
-                print("Copying " + file + "...")
-                shutil.copy(file, path)
+        if os.path.exists(src_path):
+            files_to_copy = os.listdir(src_path)
 
-                if os.isfile(os.path.join(path, file)) is not True:
-                    raise OSError("Newly generated file not found")
+            if len(files_to_copy) > 0:
+                for file in files_to_copy:
+                    print("Copying " + file + "...")
+                    shutil.copy(os.join(src_path, file), dst_path)
+
+                    if os.isfile(os.path.join(dst_path, file)) is not True:
+                        raise OSError("Newly generated file not found")
+
+        else:
+            raise OSError("Source path does not exist")
 
     print("File copy from src to public completed.")
 
