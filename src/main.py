d192118bd53d2cf6ee769fa358a52616b7d70177
Thomas Runyan <runyant13@gmail.com>
2024-07-20 08:52:36 -0400

    Added function to copy all src files to public

--- src/main.py
+++ src/main.py
@@ -1,9 +1,29 @@
-from textnode import TextNode
+import os
+import shutil
 
 
 def main():
-    textNode = TextNode('This is a text node', 'bold', 'https://www.boot.dev')
-    textNode.__repr__()
+    copy_src_to_public()
+
+
+def copy_src_to_public():
+    path = "./public"
+    if os.path.exists(path):
+        print("Deleting public dir...")
+        shutil.rmtree(path)
+    else:
+        os.mkdir(path)
+        files_to_copy = os.listdir()
+
+        if len(files_to_copy) > 0:
+            for file in files_to_copy:
+                print("Copying " + file + "...")
+                shutil.copy(file, path)
+
+                if os.isfile(os.path.join(path, file)) is not True:
+                    raise OSError("Newly generated file not found")
+
+    print("File copy from src to public completed.")
 
 
 main()
