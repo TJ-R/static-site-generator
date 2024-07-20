2d4f2565a8fdc5195a1e2e7d5f36157b0e4338f1
Thomas Runyan <runyant13@gmail.com>
2024-07-20 09:19:52 -0400

    Fixed more issues with copy src to dst function

--- src/main.py
+++ src/main.py
@@ -3,12 +3,10 @@ import shutil
 
 
 def main():
-    copy_src_to_public()
+    copy_src_to_dst("./static", "./public")
 
 
-def copy_src_to_public():
-    src_path = "./static"
-    dst_path = "./public"
+def copy_src_to_dst(src_path, dst_path):
     if os.path.exists(dst_path):
         print("Deleting public dir...")
         shutil.rmtree(dst_path)
@@ -16,20 +14,22 @@ def copy_src_to_public():
     os.mkdir(dst_path)
 
     if os.path.exists(src_path):
+        print(src_path)
         files_to_copy = os.listdir(src_path)
 
         if len(files_to_copy) > 0:
             for file in files_to_copy:
-                print("Copying " + file + "...")
-                shutil.copy(os.path.join(src_path, file), dst_path)
-
-                if os.path.isfile(os.path.join(dst_path, file)) is not True:
-                    raise OSError("Newly generated file not found")
+                if os.path.isfile(os.path.join(src_path, file)):
+                    shutil.copy(os.path.join(src_path, file), dst_path)
+                else:
+                    new_src_path = os.path.join(src_path, file)
+                    new_dst_path = os.path.join(dst_path, file)
+                    copy_src_to_dst(new_src_path, new_dst_path)
 
     else:
         raise OSError("Source path does not exist")
 
-    print("File copy from src to public completed.")
+    print(f"File copy from {src_path} to {dst_path} completed.")
 
 
 main()
